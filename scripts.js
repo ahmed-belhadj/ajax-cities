38030a5a52e969fec95503f417423efc96020b2e
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-07-24 15:08:30 -0500

    displayMatches

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 const endpoint =
   "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
 const cities = [];
+const search = document.querySelector(".search");
+const suggestions = document.querySelector(".suggestions");
 
 fetch(endpoint)
   .then(raw => raw.json())
@@ -12,3 +14,21 @@ function getMatches(input, cities) {
     return location.city.match(regex) || location.state.match(regex);
   });
 }
+
+function displayMatches() {
+  const matches = getMatches(this.value, cities);
+  const matchesHTML = matches
+    .map(location => {
+      return `
+        <li>
+            <span class="name">${location.city}, ${location.state}</span>
+            <span class="population">Population: ${location.population}</span>
+        </li>
+        `;
+    })
+    .join();
+  suggestions.innerHTML = matchesHTML;
+}
+
+search.addEventListener("change", displayMatches);
+search.addEventListener("keyup", displayMatches);
